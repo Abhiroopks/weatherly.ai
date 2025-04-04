d54b5dd1749e2ca0dbf77780f0fa062a6a7d8b8d
Abhiroop Kodandapur Sanjeeva <abhiroopks@gmail.com>
2025-04-04 12:42:48 -0400

    Add support for string address inputs

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
+from geopy.geocoders import Nominatim
 
 from directions.directions import get_directions, split_directions
 from directions.models import Coordinates
 from weather.models import Weather
 from weather.weather import get_weather
 
+
 app = FastAPI()
+GEOLOCATOR = Nominatim(user_agent="CommuteSense", timeout=10)
 
 
 @app.get("/")
@@ -19,25 +22,23 @@ def read_root() -> dict[str, str]:
 
 
 @app.get("/weather_report")
-def get_weather_report(
-    start_lat: float, start_lon: float, end_lat: float, end_lon: float
-) -> list[Weather]:
+def get_weather_report(start_address: str, end_address: str) -> list[Weather]:
     """
     Endpoint to get weather report for a route defined by starting and ending coordinates.
 
     Args:
-        start_lat: The starting latitude.
-        start_lon: The starting longitude.
-        end_lat: The ending latitude.
-        end_lon: The ending longitude.
+        start_address (str): The starting address of the route.
+        end_address (str): The ending address of the route.
 
     Returns:
         list[Weather]: A list of Weather objects containing the weather information
             for each significant point along the route.
     """
-    start = Coordinates((start_lat, start_lon))
-    end = Coordinates((end_lat, end_lon))
-    directions = get_directions(start, end)
+    start_address = GEOLOCATOR.geocode(start_address)
+    end_address = GEOLOCATOR.geocode(end_address)
+    start_coord = Coordinates((start_address.latitude, start_address.longitude))
+    end_coord = Coordinates((end_address.latitude, end_address.longitude))
+    directions = get_directions(start_coord, end_coord)
     geo_points = split_directions(directions, interval=2000)
     weather_data = get_weather(geo_points)
     return weather_data
