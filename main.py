7d776565cee4c9de8a2ab539660d262e4ab4b902
Abhiroop Kodandapur Sanjeeva <abhiroopks@gmail.com>
2025-04-18 14:19:26 -0400

    add comfort score and other route overall details to return value

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from geopy.geocoders import Nominatim
 
 from directions.directions import get_directions, split_directions
 from directions.models import Coordinates
-from weather.models import Weather
-from weather.weather import get_weather
+from weather.models import Weather, WeatherReport
+from weather.weather import generate_weather_report, get_weather
 
 app = FastAPI()
 GEOLOCATOR = Nominatim(user_agent="CommuteSense", timeout=10)
@@ -21,17 +21,16 @@ def read_root() -> dict[str, str]:
 
 
 @app.get("/weather_report")
-def get_weather_report(start_address: str, end_address: str) -> list[Weather]:
+def get_weather_report(start_address: str, end_address: str) -> WeatherReport:
     """
-    Endpoint to get weather report for a route defined by starting and ending coordinates.
+    Generates a weather report based on the given start and end addresses.
 
     Args:
-        start_address (str): The starting address of the route.
-        end_address (str): The ending address of the route.
+        start_address: The starting address for the route.
+        end_address: The ending address for the route.
 
     Returns:
-        list[Weather]: A list of Weather objects containing the weather information
-            for each significant point along the route.
+        WeatherReport: A WeatherReport object containing the weather details for the route.
     """
     start_geo = GEOLOCATOR.geocode(start_address)
     if start_geo is None:
@@ -49,4 +48,4 @@ def get_weather_report(start_address: str, end_address: str) -> list[Weather]:
 
     weather_data = get_weather(geo_points)
 
-    return weather_data
+    return generate_weather_report(weather_data)
--- weather/models.py
+++ weather/models.py
@@ -1,5 +1,7 @@
+from annotated_types import T
 from openmeteo_sdk.WeatherApiResponse import WeatherApiResponse
 from pydantic import BaseModel
+
 
 # Mapping of WMO weather codes to weather descriptions.
 WMO_WEATHER_CODES: dict[int, str] = {
@@ -33,6 +35,18 @@ WMO_WEATHER_CODES: dict[int, str] = {
     99: "Thunderstorm with heavy hail",
 }
 
+# Weights for calculating the comfort score.
+WEATHER_COMFORT_WEIGHTS: dict[str, float] = {
+    "precipitation": 0.25,
+    "apparent_temperature": 0.2,
+    "wind": 0.1,
+    "visibility": 0.3,
+    "day_night": 0.15
+    
+}
+
+# Ideal temperature in celsius (this is rather subjective).
+IDEAL_TEMP_RANGE = (20, 25)
 
 class Weather(BaseModel):
     """
@@ -50,15 +64,30 @@ class Weather(BaseModel):
         super().__init__(
             lat=weather.Latitude(),
             lon=weather.Longitude(),
-            temp=current.Variables(0).Value(),
+            apparent_temp=current.Variables(0).Value(),
             precipitation=current.Variables(1).Value(),
-            weather_code=current.Variables(2).Value(),
             weather_description=WMO_WEATHER_CODES[current.Variables(2).Value()],
+            is_day=current.Variables(3).Value(),
+            wind_gusts=current.Variables(4).Value(),
+            visibility=current.Variables(5).Value(),
         )
 
     lat: float
     lon: float
-    temp: float
+    apparent_temp: float
     precipitation: float
-    weather_code: int
     weather_description: str
+    is_day: bool
+    wind_gusts: float
+    visibility: float
+
+
+class WeatherReport(BaseModel):
+
+    max_precip: float
+    mean_temp: float
+    max_gust: float
+    min_visibility: float
+    is_day: bool    
+    comfort_score: int
+    description: str
